82108d775a7d3028a2f3539da97c963533bb160a
Cris575 <cg691539@gmail.com>
2026-07-23 20:19:24 -0600

    Simplify multiple choice questions by reducing answer options

--- course/4-async/exercises/6-async/code.js
+++ course/4-async/exercises/6-async/code.js
@@ -1,27 +1,32 @@
-function getItemData() {
-  const response = await fetch('https://api.boot.dev/v1/courses_rest_api/learn-http/items', getSettings())
-  return response.json()
-}
+(async function () {
+  async function getItemData() {
+    const response = await fetch(
+      "https://api.boot.dev/v1/courses_rest_api/learn-http/items",
+      getSettings(),
+    );
+    return response.json();
+  }
 
-// don't touch below this line
+  // don't touch below this line
 
-const items = await getItemData()
+  const items = await getItemData();
 
-logItems(items)
+  logItems(items);
 
-function getSettings() {
-  return {
-    method: 'GET',
-    mode: 'cors',
-    headers: {
-      'X-API-Key': 'Testing',
-      'Content-Type': 'application/json'
+  function getSettings() {
+    return {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "X-API-Key": "Testing",
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  function logItems(items) {
+    for (const item of items) {
+      console.log(item.name);
     }
   }
-}
-
-function logItems(items) {
-  for (const item of items) {
-    console.log(item.name)
-  } 
-}
+})();
