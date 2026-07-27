91650bdf850fb692d03ddc7f31356dc9da111871
Cris575 <cg691539@gmail.com>
2026-07-26 19:45:18 -0600

    Refactor error handling in various exercises and simplify multiple choice questions

--- course/5-errors/exercises/3-try-catch-async/code.js
+++ course/5-errors/exercises/3-try-catch-async/code.js
@@ -1,9 +1,15 @@
-const leaderboard = await fetchLeaderBoard()
-console.log(leaderboard)
+(async () => {
+  try {
+    const leaderboard = await fetchLeaderBoard();
+    console.log(leaderboard);
+  } catch (err) {
+    console.log("Our servers are down, but we will be up and running soon");
+  }
 
-// don't touch below this line
+  // don't touch below this line
 
-async function fetchLeaderBoard() {
-  const response = await fetch('https://fantasyquest.servers')
-  return response.json()
-}
+  async function fetchLeaderBoard() {
+    const response = await fetch("https://fantasyquest.servers");
+    return response.json();
+  }
+})();
--- course/5-errors/exercises/3-try-catch-async/complete.js
+++ course/5-errors/exercises/3-try-catch-async/complete.js
@@ -1,13 +1,13 @@
 try {
-  const leaderboard = await fetchLeaderBoard()
-  console.log(leaderboard)
+  const leaderboard = await fetchLeaderBoard();
+  console.log(leaderboard);
 } catch (err) {
-  console.log('Our servers are down, but we will be up and running soon')
+  console.log("Our servers are down, but we will be up and running soon");
 }
 
 // don't touch below this line
 
 async function fetchLeaderBoard() {
-  const response = await fetch('https://fantasyquest.servers')
-  return response.json()
+  const response = await fetch("https://fantasyquest.servers");
+  return response.json();
 }
